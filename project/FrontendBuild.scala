09f5a01c9ab70880e3e9980959f28d754c58c3fe
Ralf Oenning <ralf@invicto.net>
2016-10-03 11:11:51 +0100

    added wiremock as test dependency

--- project/FrontendBuild.scala
+++ project/FrontendBuild.scala
@@ -50,7 +50,8 @@ private object AppDependencies {
         "org.scalatest" %% "scalatest" % scalaTestVersion % scope,
         "org.pegdown" % "pegdown" % pegdownVersion % scope,
         "org.jsoup" % "jsoup" % "1.8.1" % scope,
-        "com.typesafe.play" %% "play-test" % PlayVersion.current % scope
+        "com.typesafe.play" %% "play-test" % PlayVersion.current % scope,
+        "com.github.tomakehurst" % "wiremock" % "1.58" % scope
       )
     }.test
   }
